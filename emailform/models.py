3c89e4a2d482717e0349edd2775c364c1d130c9d
Mary E. Ellis <mary.e.ellis@gmail.com>
2016-05-23 23:03:14 -0400

    Initial baseline for form submit using ajax and json.

---
+++ emailform/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+  first = models.CharField(max_length=50)
+  last = models.CharField(max_length=50)
+  email = models.EmailField(max_length=254)
+  created_date = models.DateTimeField(default=timezone.now)
+
+  def publish(self):
+    self.created_date()
+    self.save()
+
+  def __str__(self):
+    return self.email
