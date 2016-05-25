aee8c4bcd3bb2b10ca4890846d76ed9ab53e5efe
Erik Marty <erik.marty@mediahive.com>
2016-05-25 18:06:36 -0400

    Erik's updates

--- emailform/views.py
+++ emailform/views.py
@@ -5,32 +5,39 @@ from django.utils import timezone
 from emailform.models import Post
 import json
 
-# Create your views here.
 def main_page(request):
-  tmpl_vars = {
+    tmpl_vars = {
     'all_posts': Post.objects.all(),
     'form': PostForm()
-  }
-  return render(request, 'emailform/main_page.html', tmpl_vars)
+    }
+    #return render(request, 'emailform/main_page.html', tmpl_vars)
+    return render(request, 'email_form.html', tmpl_vars)
 
 
 def create_post(request):
     if request.method == 'POST':
         #I think something is wrong here possibly
-        post_text = request.POST.get('try_this')
+        post_text = request.POST.get('the_post')
         response_data = {}
 
-        post = Post(text=post_text, author=request.user)
+        # We do not have an author object so it will error
+        #post = Post(text=post_text, author=request.user)
+        post = Post(text=post_text)
         post.save()
 
         response_data['result'] = 'Create post successful!'
         response_data['postpk'] = post.pk
         response_data['text'] = post.text
         response_data['created'] = post.created.strftime('%B %d, %Y %I:%M %p')
-        response_data['author'] = post.author.username
+        #response_data['author'] = post.author.username
 
-        return JsonResponse(response_data)
+        # This was not doing anything
+        #return JsonResponse(response_data)
 
+        return HttpResponse(
+            json.dumps(response_data),
+            content_type="application/json"
+        )
     else:
         return HttpResponse(
             json.dumps({"nothing to see": "this isn't happening"}),
